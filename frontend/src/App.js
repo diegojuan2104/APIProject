78a707885e94bc3761408cf93be3b5f622ca041d
Juan Diego Mejía Otálvaro <diegojuan2104@hotmail.com>
2020-04-10 13:50:16 -0500

    Frontend with react start

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation';
+import formCreateLink from './components/formCreateLink';
+import formCreateUser from './components/formCreateUser';
+import LinkList from './components/LinkList';
+function App() {
+	return (
+		<div>
+			<Navigation />
+			<p>Hello world</p>
+		</div>
+	);
+}
+
+export default App;
